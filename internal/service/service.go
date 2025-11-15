9cb6be59b0181b8983ddf073589b5e3421896938
Matvey Fedorov <mfgolden@yandex.ru>
2025-11-15 22:12:29 +0300

    style: prev commit additional

--- internal/service/service.go
+++ internal/service/service.go
@@ -105,7 +105,6 @@ type PullRequestMergeOutputPR struct {
 	AuthorID          string    `json:"author_id"`
 	Status            string    `json:"status"`
 	AssignedReviewers []string  `json:"assigned_reviewers"`
-	CreatedAt         time.Time `json:"created_at"`
 	MergedAt          time.Time `json:"mergedAt"`
 }
 
@@ -115,12 +114,11 @@ type PullRequestReassignOutput struct {
 }
 
 type PullRequestReassignOutputPR struct {
-	PullRequestID     string    `json:"pull_request_id"`
-	PullRequestName   string    `json:"pull_request_name"`
-	AuthorID          string    `json:"author_id"`
-	Status            string    `json:"status"`
-	AssignedReviewers []string  `json:"assigned_reviewers"`
-	CreatedAt         time.Time `json:"created_at"`
+	PullRequestID     string   `json:"pull_request_id"`
+	PullRequestName   string   `json:"pull_request_name"`
+	AuthorID          string   `json:"author_id"`
+	Status            string   `json:"status"`
+	AssignedReviewers []string `json:"assigned_reviewers"`
 }
 
 type PullRequest interface {
